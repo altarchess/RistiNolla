d5dedd795df44dabeb9cd727719a9c2b06008c6d
Kim Kåhre <kim.kahre@helsinki.fi>
2022-10-28 02:07:44 +0300

    test

--- Engine/catch2/test.cpp
+++ Engine/catch2/test.cpp
@@ -1,20 +1,20 @@
-#include "catch.h"
+#define CONFIG_CATCH_MAIN
+#include "catch.hpp"
+#include <iostream>
 #include "../src/board.h"
 
-SCENARIO("b.makeMove activates correct movegen squares and generate generates the correct squares") {
-    GIVEN("An empty board of size 10x10") {
-        Board b;
-        b.resize(5,5);
+TEST_CASE("simple test case","[simple]"){
+	REQUIRE(1 == 1);
+};
 
-        WHEN("When a move is made on square 10") {
-            b.makeMove(7, 1);
-
-            THEN("Count of squares to generate moves for equals 8") {
-                b.generate(-1, -1);
-                int count = 0;
-                while (b.next() != -1)count++; 
-                REQUIRE(count == 8);
-            }
-        }
-    }
-}
+/*TEST_CASE("makemove", "[makemove]") {
+    std::cout << "WAFASFASF"<<  std::endl;
+    Board b;
+    initKeys();
+    b.resize(5,5);
+    b.makeMove(7, 1);
+    b.generate(-1, -1);
+    int count = 0;
+    while (b.next() != -1)count++; 
+    REQUIRE(count == 8);
+}*/
