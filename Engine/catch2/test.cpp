314df69549835b2e67cf5d68f51a39b55f76fc30
Kim Kåhre <kim.kahre@helsinki.fi>
2022-10-28 04:14:15 +0300

    ok testing stuff

--- Engine/catch2/test.cpp
+++ Engine/catch2/test.cpp
@@ -3,15 +3,11 @@
 #include <iostream>
 #include "../src/board.h"
 
-TEST_CASE("simple test case","[simple]"){
-	REQUIRE(1 == 1);
-};
+Board b;
 
-TEST_CASE("makemove", "[makemove]") {
-    std::cout << "WAFASFASF"<<  std::endl;
-    Board b;
-    initKeys();
+TEST_CASE("makemove activation of movegen squares", "[makemove]") {
     b.resize(5,5);
+    b.setActivePlayer(0);
     b.makeMove(7, 1);
     b.generate(-1, -1);
     int count = 0;
